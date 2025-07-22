ba741775a62db09505d58e8147444bddb14a2323
sam <scb40@sussex.ac.uk>
2025-07-22 18:17:57 +0100

    Update A00/estim*_spth01.m ... ... (estimFreq_spth01.m and estimBaye_spth01.m) ... ... to estimate learning models for each distinct PairId ... ... rather than each PairType. Also updated A00/spth_nll.m to accept target response field indices ... ... and actual response field indices as third and fourth inputs.

--- A00/estimBaye_spth01.m
+++ A00/estimBaye_spth01.m
@@ -9,7 +9,7 @@ function [spth01,Data,fh] = estimBaye_spth01(SubjectId,dispPlot)
 %    Estimates model parameters for a Softplus-tanh model using the von
 %    Mises distribution for a given subject. The function retrieves subject
 %    data using getSubjectData, processes the data, estimates the model
-%    parameters for different pair types via negative log-likelihood
+%    parameters for different pairs via negative log-likelihood
 %    minimization (using fmincon with MultiStart), predicts probability
 %    mass functions (PMFs) for discrete response angles, and optionally
 %    plots the prediction curves.
@@ -24,7 +24,7 @@ function [spth01,Data,fh] = estimBaye_spth01(SubjectId,dispPlot)
 %
 % Outputs:
 %    spth01 - A table containing the estimated model parameters (b0, b1)
-%             and negative log-likelihood (nll) for each pair type.
+%             and negative log-likelihood (nll) for each pair.
 %    Data   - A table containing the subject data along with predicted
 %             outcome fields.
 %    fh     - If dispPlot is true, fh is the figure handle of the plotted
@@ -53,93 +53,84 @@ for iTheta = 0:3
     Data.(['pmf',num2str(iTheta)]) = nan(size(Data,1),1);
 end
 
-%% Specify PairTypes
-PairType = categorical({'Superv';'Zeropu';'Comuns';'Nonuns'});
+%% Specify PairId and preallocate PairType
+PairId = unique(Data.PairId);
+PairType = cell(size(PairId));
 
 %% Loop through each trial type to estimate the model
-if dispPlot
-    fh = figure;
-end
-bHat = cell(numel(PairType),1);
-posterior = cell(numel(PairType),1);
-for iPairType = 1:numel(PairType)
+bHat = cell(numel(PairId),1);
+posterior = cell(numel(PairId),1);
+for iPairId = 1:numel(PairId)
 
-    % Extract rows of Data that correspond to the current pair type
-    rowSel = Data.PairType==PairType(iPairType);
+    % Extract rows of Data that correspond to the current pair ID
+    rowSel = Data.PairId==PairId(iPairId);
     sData = Data(rowSel,:);
+    PairType{iPairId} = char(sData.PairType(1));
 
     %% Extract the predictor and response variables
     x = sData.tSup;
     maxX = max(x);
-    Y = nan(numel(x),6);
+    c = nan(numel(x),1); % The targets
+    Y = nan(numel(x),6); % The actual responses
     for ii = 1:size(sData,1)
+        c(ii) = sData.FieldIdx_C(ii);
         r = sData.FieldIdx_R{ii};
         r = unique(r,'stable');
         if numel(r) < 6
             r = [r;nan(6-numel(r),1)]; %#ok<AGROW>
         end
-        Y(ii,:) = wrapTo2Pi((r'-sData.FieldIdx_C(ii)).*(pi/3));
+        Y(ii,:) = r';
     end
     Data.Y(rowSel,:) = Y;
 
     %% Estimate the model
     logPdf = @(p) ...
-    	-spth_nll(p,x,Y) ...
+    	-spth_nll(p,x,c,Y) ...
         +logUniformDist(p(1),maxX) ...
         +gampdf(p(2),1,0.05);
     hmc = hmcSampler(logPdf,[maxX/2;0.01],'UseNumericalGradient',1);
     hmc = tuneSampler(hmc); % Auto-tune step size
-    samples = drawSamples(hmc,'NumSamples',10000,'Burnin',1000,'VerbosityLevel',1);
-    bHat{iPairType} = mean(samples,1)';
-    posterior{iPairType} = samples;
+    samples = drawSamples(hmc,'NumSamples',1000,'Burnin',1000,'VerbosityLevel',1);
+    bHat{iPairId} = mean(samples,1)';
+    posterior{iPairId} = samples;
 
     %% Predict
-    [pmf,angles] = spth_pred(x,bHat{iPairType});
+    [pmf,angles] = spth_pred(x,bHat{iPairId});
     for iTheta = 0:3
         sTheta = abs(abs(wrapToPi(angles))-(pi*iTheta/3))<(1e-6);
         Data.(['pmf',num2str(iTheta)])(rowSel,:) = sum(pmf(:,sTheta),2);
-    end
-
-    %% Plot
-    if dispPlot
-        plotPreds(x,pmf,angles);
     end
 end
 
 %% Save the outputs
 b0 = cellfun(@(v)v(1),bHat);
 b1 = cellfun(@(v)v(2),bHat);
-spth01 = table(PairType,b0,b1,posterior);
-return
+spth01 = table(PairId,b0,b1,posterior);
 
-function [] = plotPreds(x,pmf,angles)
-% plotPreds plots the predicted PMFs for each discrete angle bin.
-%
-% Inputs:
-%    x      - Vector of predictor values.
-%    pmf    - Matrix of predicted probability mass functions.
-%    angles - Vector of angle values (in radians).
-%
-% This function creates a 2x2 subplot, one for each angle bin (0 to
-% 3*pi/3), and plots the predicted probability curves.
-
-persistent lineCount;
-if isempty(lineCount)
-    lineCount = 1;
-else
-    lineCount = lineCount + 1;
-end
-
-for iTheta = 0:3
-    subplot(2,2,iTheta+1);
+%% Plot
+if dispPlot
+    fh = figure;
     hold on;
-    sTheta = abs(abs(wrapToPi(angles))-(pi*iTheta/3))<(1e-6);
-    p = sum(pmf(:,sTheta),2);
-    plot(x,p);
-    title(sprintf('%s=%f',char(952),mean(abs(wrapToPi(angles(sTheta))))));
-    if (iTheta==0) && (lineCount==4)
-        legend({'Superv','Zeropu','Comuns','Nonuns'},'Location','best');
+    for iPairId = 1:numel(PairId)
+        rowSel = Data.PairId==PairId(iPairId);
+        sData = Data(rowSel,:);
+        x = sData.tSup;
+        y = sData.pmf0;
+        if sData.PairType(1)==categorical({'Zeropu'})
+            p1 = plot(x,y,'Color','black','LineStyle','-', ...
+                'DisplayName','Zeropu');
+        elseif sData.PairType(1)==categorical({'Superv'})
+            p2 = plot(x,y,'Color','red','LineStyle','-', ...
+                'DisplayName','Superv');
+        elseif sData.PairType(1)==categorical({'Comuns'})
+            p3 = plot(x,y,'Color','green','LineStyle','-', ...
+                'DisplayName','Comuns');
+        else
+            p4 = plot(x,y,'Color','blue','LineStyle','-', ...
+                'DisplayName','Nonuns');
+        end
     end
+    legend([p1,p2,p3,p4]);
 end
 return
 
--- A00/spth_nll.m
+++ A00/spth_nll.m
@@ -1,9 +1,9 @@
-function [nll] = spth_nll(b,x,theta)
+function [nll] = spth_nll(b,x,c,Y)
 % spth_nll.m
 % Sam Berens (s.berens@sussex.ac.uk)
-% 24/03/2025
+% 22/07/2025
 %
-% Syntax:  nll = spth_nll(b, x, theta)
+% Syntax:  nll = spth_nll(b, x, c, Y)
 %
 % Description:
 %    Computes the negative log-likelihood (nll) for a Softplus-tanh model
@@ -11,26 +11,26 @@ function [nll] = spth_nll(b,x,theta)
 %    predictor values, and observed angle responses.
 %
 % Inputs:
-%    b     - A 2-element vector of model parameters.
-%    x     - Vector of predictor values.
-%    theta - Matrix of observed angle responses (in radians) for each 
-%            trial.
+%    b   - A 2-element vector of model parameters.
+%    x   - Vector of predictor values.
+%    c   - Vector of target (correct) responses (as field indices).
+%    Y   - Matrix of observed responses (field indices) for each trial.
 %
 % Outputs:
-%    nll   - The computed negative log-likelihood value.
+%    nll - The computed negative log-likelihood value.
 %
 % Example:
-%    nll = spth_nll([1, 0.5], x, theta);
+%    nll = spth_nll([1, 0.5], x, c, Y);
 %
 % See also: spth_pred
 %
-
+Theta = wrapTo2Pi((Y-repmat(c,1,6)).*(pi/3));
 n = numel(x);
 [pmf,angles] = spth_pred(x,b);
 nll = nan(n,1);
 for iPred = 1:n
     p = pmf(iPred,:);
-    t = theta(iPred,:);
+    t = Theta(iPred,:);
     t = t(~isnan(t));
     y = arrayfun(@(tt)find(abs(tt-angles)<1e-6),t);
     ty = y(1:(end-1));
