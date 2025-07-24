db926954272fd89ad0500f76da20930c98df441a
sam <scb40@sussex.ac.uk>
2025-07-24 16:10:09 +0100

    Added A00/z01_makeStanInputs.m ... ... and updated A00/z00_makeTrainTaskIO.m.

--- A00/z00_makeTrainTaskIO.m
+++ A00/z00_makeTrainTaskIO.m
@@ -1,11 +1,30 @@
 function [] = z00_makeTrainTaskIO()
-dirLits = dir('../../Data');
+% z00_makeTrainTaskIO.m
+% Sam Berens (s.berens@sussex.ac.uk)
+% 24/07/2025
+%
+% Syntax:  z00_makeTrainTaskIO()
+%
+% Description:
+%    Ensures that subject-specific training data (in TrainTaskIO) have been
+%    retrieved from a web server, pre-processed and saved to a local file.
+%
+% Inputs:
+%    NONE
+%
+% Outputs:
+%    NONE
+%
+% Example:
+%    z00_makeTrainTaskIO();
+%
+dirLits = dir(['..',filesep,'..',filesep,'Data']);
 subjectIds = {dirLits(cellfun(@(s)numel(s)==8,{dirLits.name}')).name}';
 subjectIds = categorical(subjectIds);
 fh = waitbar(0,'Saving data...');
 for iSubject = 1:numel(subjectIds)
-    sId = subjectIds(iSubject);
-    [~] = getTrainTaskIO(char(sId));
+    sId = char(subjectIds(iSubject));
+    [~] = getTrainTaskIO(sId);
     waitbar(iSubject/numel(subjectIds),fh);
 end
 close(fh);
