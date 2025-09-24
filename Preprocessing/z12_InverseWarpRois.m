41c7ead42c15fb1d7330613bcfefe45e396077fe
sam <scb40@sussex.ac.uk>
2025-09-24 19:55:11 +0100

    Updated Preprocessing/z12_InverseWarpRois.m

--- Preprocessing/z12_InverseWarpRois.m
+++ Preprocessing/z12_InverseWarpRois.m
@@ -1,125 +1,136 @@
-function [] = z12_InverseWarpRois(G)
-%%% NOTE THIS NEEDS RUNNING AGAIN. AND THIS NEEDS WORK
+function [] = z12_InverseWarpRois(G,roisToWarp)
+% Inverse warp ROIs from MNI space to native space (for each participant).
+% This procedure uses DARTEL tools and so requires a DARTEL group template,
+% along with flowfield images per participant. This routine involves three
+% distinct steps:
+%   1) Performing an affine transformation of the ROI images so they are
+%      in registration with the DARTEL group template (by manipulating the
+%      image headers alone).
+%   2) Calling the "tools.dartel.crt_iwarped" utility in SPM to perform the
+%      non-linear warp.
+%   3) Moving the inverse-warped files into a dedicated NativeRoi directory
+%      (see code for details).
+% Note that the resulting inverse normalised images have the same
+% dimensions as the flowfields.
+%
 
-error('This script needs work');
-
-% Create inverse normalised versions of some image(s). The image that is inverse-normalised should
-% be in alignment with the template (generated during the warping procedure). Note that the results
-% have the same dimensions as the “flow fields”, but are mapped to the original images via the affine
-% transformations in their headers.
-
-%ergo in order to warp our MNI ROIs to native space we need to get them in
-%alignment with the group template before using the dartel.inverseWarp
-%procedure (using each subjects flowfields)
-
-
-%to do this we could just do coregistration on the ROIs with the template
-%as the reference image? - or we could use the formula below
-
-pathData = ['..',filesep,'..',filesep,'Data'];
-pathSource = [pathData,filesep,'_Group',filesep,'MniRois'];
-pathTemplates = [pathData,filesep,'_Group',filesep,'Structural',filesep,'DARTEL_Templates']
-grpRoiDir = [drtlDir, filesep, 'GroupSpaceRois'];
-
-if ~exist(grpRoiDir, "dir")
-    mkdir(grpRoiDir);
+%% Input checks
+if nargin < 2
+    roisToWarp = {};
+    % This will be populated will all the native ROIs that match the 
+    % following filename schema:
+    %    ../../Data/_Group/MniRois/MniRois/_*.nii
+elseif nargin < 1
+    error('You must provide a group ID (string).');
+end
+if ischar(roisToWarp)
+    roisToWarp = {roisToWarp};
+elseif ~iscell(roisToWarp)
+    error(...
+    'roisToWarp input must be either a char or a cell array of chars.');
 end
 
-%find group template
-srcDartelTemplate = dir([drtlDir,filesep,'*G1_Template_6.nii']);
-dartelTemplate = [srcDartelTemplate.folder,filesep,srcDartelTemplate.name];
+%% Get subjectsIds
+subjectIds = getSubjectIds(G);
 
+%% Set the paths structure
+paths.data = ['..',filesep,'..',filesep,'Data'];
+paths.source = [paths.data,...
+    filesep,'_Group',...
+    filesep,'MniRois'];
+paths.templates = [paths.data,...
+    filesep,'_Group',...
+    filesep,G,...
+    filesep,'Structural',...
+    filesep,'DARTEL_Templates'];
+paths.groupRois = [paths.data,...
+    filesep,'_Group',...
+    filesep,G,...
+    filesep,'Structural',...
+    filesep,'GroupRois'];
 
-subjectIds = getG1();
-nSubs = numel(subjectIds);
-nativeDirs = cell(nSubs);
-for ii=1:nSubs
-    subjDir = [dataDir,filesep,subjectIds{ii}];
-    struDir = [subjDir,filesep,'Structural'];
-    nativeDirs{ii} = [struDir,filesep,'NativeRois'];
-    if ~exist(nativeDirs{ii},'dir')
-        mkdir([nativeDirs{ii},filesep,'w'])
-        mkdir([nativeDirs{ii},filesep,'rw']) %for later sliced rois to go in
-        mkdir([nativeDirs{ii},filesep,'mrw']) %for later masked rois to go in
+% Loop through the participants to get the locations of the flowfields ...
+% ... and the target location of the warped native ROIs.
+paths.flowfields = cell(size(subjectIds));
+paths.targets = cell(size(subjectIds));
+for iSubject = 1:numel(subjectIds)
+    paths.flowfields{iSubject} = [paths.data,...
+        filesep,subjectIds{iSubject},...
+        filesep,'Structural',...
+        filesep,G];
+    paths.targets{iSubject} = [paths.flowfields{iSubject},...
+        filesep,'NativeRois',...
+        filesep,'w'];
+end
+
+%% Set the fns structure
+fns.template = dir([paths.templates,filesep,'*_Template_6.nii']);
+fns.template = [fns.template.folder,filesep,fns.template.name];
+fns.group2Mni = dir([paths.templates,filesep,'*_Template_6_2mni.mat']);
+fns.group2Mni = [fns.group2Mni.folder,filesep,fns.group2Mni.name];
+if isempty(roisToWarp)
+    dirList = dir([paths.source,filesep,'_*.nii']);
+    roisToWarp = {dirList.name}';
+end
+fns.roisToWarp = cellfun(@(s)[paths.source,filesep,s],...
+    roisToWarp,'UniformOutput',false);
+fns.groupRois = cellfun(@(s)[paths.groupRois,filesep,s],...
+    roisToWarp,'UniformOutput',false);
+
+% Loop through participants to get the names of the flowfields
+fns.flowfields = cell(size(subjectIds));
+for iSubject = 1:numel(subjectIds)
+    dirList = dir(...
+        [paths.flowfields{iSubject},filesep,'u_*_Template_6.nii']);
+    fns.flowfields{iSubject} = [dirList.folder,filesep,dirList.name];
+end
+
+%% 1) Affine transform the MNI ROIs
+% M1 is the direction cosine matrix of the ROI in MNI space:
+%   VxROI -> MNI
+% M2 is the *rotation matrix* that moves the DARTEL template to MNI space:
+%   GroupSpace -> MNI (we want the inverse of this);
+% M3 is the direction cosine matrix of the DARTEL template in group space:
+%   VxTemplate -> GroupSpace
+% M4 is the direction cosine matrix of the ROI in group space:
+%   VxROI -> GroupSpace
+%   M4 = M3*inv(M2)*M1;
+
+% First we load M2 and M3
+M2 = load(fns.group2Mni);
+M2 = M2.mni.affine;
+M3 = spm_vol(fns.template);
+M3 = M3.mat;
+
+% Now loop through ROIs to transform .mat and write
+for iRoi = 1:numel(roisToWarp)
+    V1 = spm_vol(fns.roisToWarp{iRoi});
+    Y1 = spm_read_vols(V1);
+    V1.fname = fns.groupRois{iRoi};
+    V1.descrip = ['Affined2Group ',V1.descrip];
+    M1 = V1.mat;
+    M4 = M3*M2\M1;
+    V1.mat = M4;
+    spm_write_vol(V1,Y1);
+end
+
+%% Create and execute SPM batch
+spmBatch = {};
+spmBatch{1}.spm.tools.dartel.crt_iwarped.flowfields = fns.flowfields;
+spmBatch{1}.spm.tools.dartel.crt_iwarped.images = fns.groupRois;
+spmBatch{1}.spm.tools.dartel.crt_iwarped.K = 9;
+spmBatch{1}.spm.tools.dartel.crt_iwarped.interp = 7;
+spm_jobman('initcfg');
+spm_jobman('run',spmBatch);
+
+%% Loop through participants to move the inverse warped ROIs
+for iSubject = 1:numel(subjectIds)
+    if ~exist(paths.targets{iSubject},'dir')
+        mkdir(paths.targets{iSubject});
     end
+    movefile(...
+        [paths.flowfields{iSubject},filesep,'w_*.nii'],...
+        paths.targets{iSubject});
 end
 
-%M is the mapping from the voxel grid of the ROI to group space
-% it is calculated as the path from
-% roiVoxelGrid->MNIspace(M1) to...
-% MNIspace->groupTemplateVoxelGrid(M3^-1) to...
-% groupTemplateVoxelGrid->groupSpace(M2)
-
-%it is neccesary to * by M2 in order to make M map the ROI voxel grid map
-%to group space, otherwise M1*inv(M3) would just tell spm to align the ROIs
-%according to the voxel grid (e.g. the intensity of voxel [1,1,1](which
-%should be black, hailing from the bottom left corner below your head)being
-%presented near the origin of the image.
-
-%the reason that these .mat files are important despite us not displaying
-%the images is that the volumes need to be aligned to group space for the
-%flowfields to work.
-
-% for formula discussion see https://www.jiscmail.ac.uk/cgi-bin/webadmin?A2=ind1203&L=spm&P=R89160&1=spm&9=A&J=on&d=No+Match%3BMatch%3BMatches&z=4
-
-%load the native2group2MNI .mat file (M3)
-affineFn = [drtlDir,filesep,'G1_Template_6_2mni.mat'];
-
-%load in the rois (for M1)
-mRoisrc = dir([mRoiDir,filesep,'_*.nii']);
-mRois = cellfun(@(x) x,{mRoisrc.name}','UniformOutput',false);
-
-%M2 is the mapping of the group template voxel grid to group space
-M2 = spm_get_space(dartelTemplate);
-%M3 is the mapping of the group template voxel grid to MNI space
-load(affineFn);
-M3 = mni.affine;
-
-%make initial group-space ROIS
-for ii=1:numel(mRois)
-    roiFilename = [mRoiDir,filesep,mRois{ii}];
-    gspaceRoiFn = [grpRoiDir,filesep,'w',mRois{ii}]; 
-    copyfile(roiFilename,gspaceRoiFn);
-%M1 is the mapping of the roi voxel grid to MNI space
-    M1 = spm_get_space(roiFilename);
-%M is the mapping from the voxel grid of the ROI to group space   
-    M = M2*inv(M3)*M1;
-%write the affine .mat to the group-space copy of the given roi volume
-    spm_get_space(gspaceRoiFn,M)
-end 
-
-%% Individually warp the rois to native space for each person
-%using their flowfield
-
-%collect the grpRois for batch warping
-srcGrpRoiDir = dir([grpRoiDir,filesep,'w_*.nii']);% should all be named this after creation above
-grpRois = cellfun(@(x,y) [x,filesep,y],{srcGrpRoiDir.folder}',{srcGrpRoiDir.name}','UniformOutput',false);
-
-for ii=1:nSubs
-    subjDir = [dataDir,filesep,subjectIds{ii}];
-    sStruDir = [subjDir,filesep,'Structural'];
-    sG1Dir = [sStruDir,filesep,'G1'];
-    srcFf = dir([sG1Dir,filesep,'u_rc1_*.nii']);
-    flowField = [srcFf.folder,filesep,srcFf.name];
-
-    spmBatch{1}= {};
-    spmBatch{1}.spm.tools.dartel.crt_iwarped.flowfields = {flowField};
-    spmBatch{1}.spm.tools.dartel.crt_iwarped.images = grpRois;
-    spmBatch{1}.spm.tools.dartel.crt_iwarped.K = 6; %default
-    spmBatch{1}.spm.tools.dartel.crt_iwarped.interp = 7; %putting it on highest for accuracy
-
-    spm_jobman('initcfg');
-    spm_jobman('run',spmBatch);
-
-    targetfld =  [nativeDirs{ii}, filesep, 'w'];
-    %move all the native-warped ROIs for that subject from where they get sent(their G1 folder because thats where the flowfield is?) to their native ROI folder
-    movefile([sG1Dir,filesep,'ww_*.nii'],targetfld)
-end
-
-
-%instead of reslicing ROIs into EPI resolution now, we will leave these unbinarised
-% and will toggle a threshold for inclusion in the final analaysis rois
-% masks in a later script (will need to use interpolation but can do this
-% with spm function e.g. reslice?
-
-
+return
