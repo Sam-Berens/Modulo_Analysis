bf1f1a150470c2cbc2b3c7d5966ea2e0dd48afea
sam <scb40@sussex.ac.uk>
2026-01-05 14:59:20 +0000

    Added checks to Tools/getNativeRoiPath.m

--- Tools/getNativeRoiPath.m
+++ Tools/getNativeRoiPath.m
@@ -16,6 +16,15 @@ roiPath = dir([dirs.Roi,filesep,'*',roiPattern,'*']);
 if ~numel(roiPath)
     roiPath = dir([dirs.Roi,filesep,roiPattern,'*']);
 end
+
+if numel(roiPath) == 0
+    error('Bad ROI pattern.');
+end
+
+if numel(roiPath) > 1
+    error('Ambiguous ROI pattern.');
+end
+
 roiPath = [roiPath.folder, filesep, roiPath.name];
 
 return
