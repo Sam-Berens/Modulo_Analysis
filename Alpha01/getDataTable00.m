331494eaaf930a5605c9f3c724d54b222b8b2645
sophie <sj483@sussex.ac.uk>
2026-01-07 15:59:08 +0000

    Rename Alpha01/DataTable01 to Alpha01/DataTable00

---
+++ Alpha01/getDataTable00.m
@@ -0,0 +1,34 @@
+function [DataTable00] = getDataTable00(G)
+
+if exist('DataTable00.mat','file')
+    DataTable00 = load('DataTable00.mat');
+    DataTable00 = DataTable00.DataTable01;
+    return
+end
+
+pNonc = get_pNonc(G);
+pNonc.cpNonc = pNonc.pNonc - mean(pNonc.pNonc);
+[roiIds,roiNames] = getRoiList();
+DataTable00 = [];
+for iRoi = 1:numel(roiIds)
+    cRoiId = roiIds{iRoi};
+    cRoiName = roiNames{iRoi};
+    PatternSim = getPatternSim(G,cRoiId);
+    hemi = double(strcmp(cRoiName(1),'r'))*2 - 1;
+    roiInfo = table(...
+        repmat(hemi,size(PatternSim,1),1),...
+        repmat(categorical({cRoiName}),size(PatternSim,1),1),...
+        'VariableNames',{'hemisphere','roiName'});
+    PatternSim = [roiInfo,PatternSim]; %#ok<AGROW>
+    PatternSim = outerjoin(pNonc,PatternSim);
+    % Rename subjectId after outerjoin
+    PatternSim.subjectId_PatternSim = [];
+    PatternSim.Properties.VariableNames{1} = 'subjectId';
+    DataTable00 = [DataTable00;PatternSim]; %#ok<AGROW>
+end
+
+% Sort by subjectId
+DataTable00 = sortrows(DataTable00,'subjectId');
+
+save('DataTable00.mat','DataTable00');
+return
