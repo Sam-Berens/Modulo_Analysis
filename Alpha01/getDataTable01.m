ad6dd0f62c293207f9272f68c2a3ed09fb119536
sophie <sj483@sussex.ac.uk>
2026-01-07 16:01:00 +0000

    Rename Alpha01/DataTable02 to Alpha01/DataTable01

--- Alpha01/getDataTable01.m
+++ Alpha01/getDataTable01.m
@@ -1,34 +1,55 @@
-function [DataTable01] = getDataTable01(G)
+function [l,r] = getDataTable01()
+G = 'G1';
+subjectIds = getSubjectIds(G);
+nSubs = numel(subjectIds);
+Omega.l = nan(nSubs,1);
+Omega.r = nan(nSubs,1);
+% loop through subject
+for iSubject=1:nSubs
+        qFname = getQfileList(subjectId);
+        %warp to MNI space
+        targetFolder = norm2MNI(G,subjectId,qFname);
+        %load q image for left and right
+        qDir = dir(targetFolder,filesep,'wq*.nii');
+        qPaths = cellfun(@(x) fullfile(x.folder,x.name),qDir,'UniformOutput',false);
+hemi = {'l'; 'r'};
+    for iHem=1:2
+        cHemi = hemi{iHem};
+        roiId = [cHemi,'HippC']; %this is the combined head body and tail
+        %this is both right and left
+        %TO DO read in q image and mask
+        
+        Q.(cHemi) = M(mniRoi.idx);
+        Y.(cHemi) = mniRoi.idx * V.mat%% TO DO this is not how to do it just place holder !! 
+        % TO DO - 
+        % zX0 - make q
+        % zX1_... = make norm images loop into seperar function which is like
+        % getDataTable01... =  %TO DO load and apply roi masks! (using roi
+        % filtering helper function)(this is where you do left and right
+        % seperately ), also collecting up qs and ys and testing their corr
+        % put that in the datatable and then ttest on datatable across
+        % subjects
+    end
 
-if exist('DataTable01.mat','file')
-    DataTable01 = load('DataTable01.mat');
-    DataTable01 = DataTable01.DataTable01;
-    return
+        Omega.l(iSubject) = corr(ql,yl);
+        Omega.r(iSubject) = corr(qr,yr);
+   
 end
+%TO DO BUILD OMEGA INTO TABLE INSTEAD OF STRUCT
+return
 
-pNonc = get_pNonc(G);
-pNonc.cpNonc = pNonc.pNonc - mean(pNonc.pNonc);
-[roiIds,roiNames] = getRoiList();
-DataTable01 = [];
-for iRoi = 1:numel(roiIds)
-    cRoiId = roiIds{iRoi};
-    cRoiName = roiNames{iRoi};
-    PatternSim = getPatternSim(G,cRoiId);
-    hemi = double(strcmp(cRoiName(1),'r'))*2 - 1;
-    roiInfo = table(...
-        repmat(hemi,size(PatternSim,1),1),...
-        repmat(categorical({cRoiName}),size(PatternSim,1),1),...
-        'VariableNames',{'hemisphere','roiName'});
-    PatternSim = [roiInfo,PatternSim]; %#ok<AGROW>
-    PatternSim = outerjoin(pNonc,PatternSim);
-    % Rename subjectId after outerjoin
-    PatternSim.subjectId_PatternSim = [];
-    PatternSim.Properties.VariableNames{1} = 'subjectId';
-    DataTable01 = [DataTable01;PatternSim]; %#ok<AGROW>
-end
+%% TO DO: 
 
-% Sort by subjectId
-DataTable01 = sortrows(DataTable01,'subjectId');
+%ADD to the markdown for the analysis folder explaining that mdl00 is
+%prereg, mdl01 is searchlight version of mdl00 and mdl02 is this here. 
+%inside mdl00.mlx file use .1 etc to denote added terms
 
-save('DataTable01.mat','DataTable01');
+
+function [qFnames] = getQfileList(subjectId)
+dirs.Data = '../../Data';
+dirs.Subject = [dirs.Data,filesep,char(subjectId)];
+dirs.Alpha01 = [dirs.Subject,filesep,'Analysis',filesep,'Alpha01'];
+dirs.Mdl02 = [dirs.Alpha01,filesep,'Mdl02'];
+qs = dir([dirs.Mdl02,filesep,'q*.nii']);
+qFnames = cellfun(@(x)[x.folder,filesep,x.name],qs,'UniformOutput',false);
 return
