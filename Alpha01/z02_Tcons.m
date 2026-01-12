8e5edcf3520b677af43e82df7c82ef799030f458
sam <scb40@sussex.ac.uk>
2026-01-12 11:37:32 +0000

    Cleanup of Alpha01/z02_Tcons.m

--- Alpha01/z02_Tcons.m
+++ Alpha01/z02_Tcons.m
@@ -1,18 +1,16 @@
 function [] = z02_Tcons(G)
 subjectIds = getSubjectIds(G);
-subjectIds = cellstr(subjectIds);
 dirs.Data = ['..',filesep,'..',filesep,'Data'];
-for iSubject=1:numel(subjectIds)
+for iSubject = 1:numel(subjectIds)
 
-    cId = subjectIds{iSubject};
+    cId = char(subjectIds(iSubject));
     dirs.Subject = [dirs.Data,filesep,cId];
     dirs.Alpha01 = [dirs.Subject,filesep,'Analysis',filesep,'Alpha01'];
 
-    %loop through the regs of intrst 0:5 and calculate their
-    % tstatistics (in a contrast which scales the beta for each run
+    % Loop through each position and stimulus to compute the t-stats
     pos = 'ab';
-    for iStim=0:5
-        for iPos=1:2
+    for iPos = 1:2
+        for iStim = 0:5
             stimId = sprintf('%s%i',pos(iPos),iStim);
             spmMat = [dirs.Alpha01,filesep,stimId,filesep,'SPM.mat'];
             spmJob{1}.spm.stats.con.spmmat = {spmMat};
@@ -20,7 +18,7 @@ for iSubject=1:numel(subjectIds)
             
             % Weights is the unpadded weight vector.
             % It relies on the design matrix being organised such that the
-            % stimulus of intrst is always encoded by the first column.
+            % stimulus of intrest is always encoded by the first column.
             spmJob{1}.spm.stats.con.consess{1}.tcon.weights = 1;
             spmJob{1}.spm.stats.con.consess{1}.tcon.sessrep = 'replsc';
             spmJob{1}.spm.stats.con.delete = 1;
