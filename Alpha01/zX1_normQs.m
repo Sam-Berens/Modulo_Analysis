eaceecd74d143044c2af74cf19f55a1c426b8328
sophie <sj483@sussex.ac.uk>
2026-01-12 10:37:15 +0000

    Continued work on zX1_normQs.m

--- Alpha01/zX1_normQs.m
+++ Alpha01/zX1_normQs.m
@@ -4,7 +4,12 @@ G = 'G1';
 subjectIds = getSubjectIds(G);
 nSubs = numel(subjectIds);
 for iSubject=1:nSubs
-    subjectId = subjectIds(iSubject);
-    norm2MNI(G,subjectId);
+    subjectId = char(subjectIds(iSubject));
+    trgtFld = fullfile(dirs.Data,subjectId,'Analysis','Alpha01',...
+    'Mdl02');
+    trgNames = {'q.nii','error.nii','b1.nii','b0.nii'};
+    imagePaths = cellfun(@(x) fullfile(trgtFld,x), trgNames,...
+        'UniformOutput',false);
+    norm2MNI(G,subjectId,imagePaths');
 end
 return
