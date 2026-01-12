20552c76581f2faec2ea31ba425e7a748e8f0cb2
sophie <sj483@sussex.ac.uk>
2026-01-12 13:42:44 +0000

    Restored previous naming convention of Datatables

--- Alpha01/getDataTable01.m
+++ Alpha01/getDataTable01.m
@@ -1,13 +1,7 @@
-function [dt2] = getDataTable02()
+function [dt1] = getDataTable01()
 G = 'G1';
 subjectIds = getSubjectIds(G);
 nSubs = numel(subjectIds);
-
-%preallocate columns for table
-y.l = nan(nSubs);
-y.r = nan(nSubs);
-q.l = nan(nSubs);
-q.r = nan(nSubs);
 
 %get MNI masks
 dirs.Data = '../../Data';
@@ -20,32 +14,44 @@ for iHem=1:2
     fName = [cDir.folder,filesep,cDir.name];
     roi.V{iHem} = spm_vol(fName);
     roi.M{iHem} = spm_read_vols(roi.V{iHem});
+    roi.idx{iHem} = find(roi.M{iHem}>0.5);
 end
+
+%preallocate columns for table
+nVx.l = numel(roi.idx{1});
+nVx.r = numel(roi.idx{2});
+y.l = nan(nSubs,nVx.l);
+y.r = nan(nSubs,nVx.r);
+q.l = nan(nSubs,nVx.l);
+q.r = nan(nSubs,nVx.r);
+
+%to do : need to filter out the idx that there is no Q for?
+
 
 % loop through subject
 for iSubject=1:nSubs
+    subjectId = subjectIds(iSubject);
     for iHem=1:2
         cHemi = hemi{iHem};
         roiId = [cHemi,'HippC']; %this is the combined head body and tail
         %load in q and mask out with roi
-        q.(cHemi)(iSubject)  = (getNormdQ(char(subjectId))).*roi.M{iHem};
-        %flatten q values
-
+        cq = getNormdQ(char(subjectId)).M;
+        q.(cHemi)(iSubject,:)  = cq(roi.idx{iHem});
         %TO DO - obtain list of mm y coords for each q values
-        roiIdx = find(roi.M{iHem}>0.5);
-        [~,y,~] = ind2sub(size(roi.M{iHem}),roiIdx);
-        y.(cHemi)(iSubject) = y .* roi.V{iHem}.mat;
+        [~,y,~] = ind2sub(size(roi.M{iHem}),roi.idx{iHem});
+        y.(cHemi)(iSubject,:) = y .* roi.V{iHem}.mat;
+        %do corr on each sub
     end
 end
 
-dt2 = table(subjectIds,y.l,q.l,y.r,q.r);
-save('Datatable02.mat',"dt2");
+% dt1 = table(subjectIds,corVals);
+%save('Datatable01.mat',"dt1");
 
 return
 
 function [q] = getNormdQ(subjectId)
 dirs.Data = '../../Data';
-qFname = fullfile(dirs.Data,subjectId,'Analysis','Alpha01','Mdl02','G1','wq.nii');
+qFname = fullfile(dirs.Data,subjectId,'Analysis','Alpha01','Mdl01','G1','wq.nii');
 q.V = spm_vol(qFname);
 q.M = spm_read_vols(q.V);
 return
