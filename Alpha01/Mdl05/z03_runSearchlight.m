7499b408b82ad9e66046b55b54e5a2d02671d35a
sam <scb40@sussex.ac.uk>
2026-03-18 15:04:05 +0000

    Update + Fix: Alpha01/Mdl05/z03_runSearchlight.m

--- Alpha01/Mdl05/z03_runSearchlight.m
+++ Alpha01/Mdl05/z03_runSearchlight.m
@@ -1,9 +1,14 @@
 function [] = z03_runSearchlight(G)
-wd = pwd;
-%Back up into Alpha01
-cd ..
+
+% Searchlight radius
+r = 5;
+
 % Get the list of subjectIds
 subjectIds = getSubjectIds(G);
+
+% Move up into Alpha01
+wd = pwd;
+cd ..;
 
 % Set the path to the /Data directory
 dirs.Data = ['..',filesep,'..',filesep,'Data'];
@@ -20,10 +25,10 @@ for iSubject = 1:numel(subjectIds)
     % Get the Timags (native space)
     Timgs = getTimgs(cSid);
 
-    %Get subject's image perm, undo zero-ordering
-    imgPerm = getImagePerm(char(cSid));
+    % Get subject's image perm, undo zero-ordering
+    imgPerm = getImgPerm(char(cSid));
     imgPerm = imgPerm + 1;
-    
+
     % Set the output directory
     dirs.Subject = [dirs.Data,filesep,char(cSid)];
     dirs.Alpha01 = [dirs.Subject,filesep,'Analysis',filesep,'Alpha01'];
@@ -32,42 +37,41 @@ for iSubject = 1:numel(subjectIds)
         mkdir(dirs.Mdl05);
     end
 
-    r = 5;
     % Set the colocation = -1 output header
     Vn = Mask.V;
     Vn.fname = [dirs.Mdl05,filesep,'zTemplate_colocation=-1.nii'];
     Vn.dt(1) = 64;
-    Vn.descrip = sprintf('Alpha01 zTemplate searchlight: colocation=-1, r=%i',r);
+    Vn.descrip = sprintf(...
+        'Alpha01 Mdl05 searchlight: colocation=-1, r=%i',r);
 
     % Set the colocation = +1 output header
     Vp = Mask.V;
     Vp.fname = [dirs.Mdl05,filesep,'zTemplate_colocation=+1.nii'];
     Vp.dt(1) = 64;
-    Vp.descrip = sprintf('Alpha01 zTemplate searchlight: colocation=+1, r=%i',r);
+    Vp.descrip = sprintf(...
+        'Alpha01 Mdl05 searchlight: colocation=+1, r=%i',r);
 
     % Set the visSim output header
     Vv = Mask.V;
     Vv.fname = [dirs.Mdl05,filesep,'zTemplate_visSim.nii'];
     Vv.dt(1) = 64;
-    Vv.descrip = sprintf('Alpha01 zTemplate searchlight: visSim, r=%i',r);
+    Vv.descrip = sprintf(...
+        'Alpha01 Mdl05 searchlight: visSim, r=%i',r);
 
     % Set the neighborhood size image
     Vc = Mask.V;
     Vc.fname = [dirs.Mdl05,filesep,'hoodSize.nii'];
     Vc.dt(1) = 16;
     Vc.descrip = sprintf(...
-        'Alpha01 zTemplate searchlight: neighborhood size (#vx), r=%i',r);
-
-    %this is a beta for coloc-1, coloc+1 and VisStruct
-    Ydepth = 3;
+        'Alpha01 Mdl05 searchlight: neighborhood size (#vx), r=%i',r);
 
     % Run the searchlight
     [Z,N] = searchlight3D(...
-        r,...         Radius   -
-       @(M) mdl5Func(M,imgPerm),... Function
-        Mask,...       Mask
-        Timgs,...      Data
-        Ydepth,...     Output depth
+        r,...     Radius
+        @(M)      mdl5Func(M,imgPerm),...
+        Mask,...  Mask
+        Timgs,... Data
+        3,...     Output depth
         char(cSid));
 
     % Save the results
@@ -79,66 +83,86 @@ for iSubject = 1:numel(subjectIds)
     spm_write_vol(Vv,Mv);
     spm_write_vol(Vc,N);
 end
-%return to Mdl04 folder
+
+% Return to the working directory
 cd(wd);
 return
 
 
 function [z] = mdl5Func(M,imgPerm)
 
-persistent prev_imgPerm S IX;
-if isempty(prev_imgPerm) || isempty(S) || isempty(IX) || ...
-    ~isequal(prev_imgPerm, imgPerm)
-        
-    % Construct the basic 6x6 similarity hypothesis
-    simFun = @(x,y) 1-((min(mod((x-y),6),mod((y-x),6))/3));
-    H_ = nan(6);
-    for ii = 1:36
-        [x,y] = ind2sub([6,6],ii);
-        H_(ii) = simFun(x,y);
+    persistent nanzscore VisSim prev_imgPerm S IX;
+    if isempty(nanzscore) || isempty(VisSim)
+        nanzscore = @(v) (v-mean(v,'omitmissing')) ./ std(v,'omitmissing');
+
+        temp = load('VisualSim.mat');
+        VisSim = temp.VisualSim;
     end
-    h = H_(tril(true(6),-1));
 
-    % Make the selectors for the real data RSM
-    S.n.ab = logical(kron([0,0;1,0],tril(true(6),-1)));
-    S.n.ba = logical(kron([0,0;1,0],triu(true(6),1)));
-    S.p.a = logical(kron([1,0;0,0],tril(true(6),-1)));
-    S.p.b = logical(kron([0,0;0,1],tril(true(6),-1)));
+    if isempty(prev_imgPerm) || isempty(S) || isempty(IX) || ...
+            ~isequal(prev_imgPerm, imgPerm)
 
-    % Load in predicted visual similarity from denseNet-169
-    V = getVisCorr();
-    
-    % Permute the rows and cols of V
-    PV = V(imgPerm,imgPerm);
-    %select out lower tri
-    v = PV(tril(true(6),-1));
-    %scale all vectors such that they are projecting from the origin
-    %and are zscored
-    h = zscore(h);
-    h = repmat(h,[2,1]);
-    v = zscore(v);
-    v = repmat(v,[4,1]);
+        % Construct the basic 6x6 similarity hypothesis
+        simFun = @(x,y) 1-((min(mod((x-y),6),mod((y-x),6))/3));
+        H_ = nan(6);
+        for ii = 1:36
+            [x,y] = ind2sub([6,6],ii);
+            H_(ii) = simFun(x,y);
+        end
 
-    % Construct the design matrix (X)
-    X = [kron(eye(2),h),v];
+        % Hypothesis for colocation=-1
+        Hn = kron(ones(2),H_);
+        Hn(logical(kron([1,1;0,1],ones(6)))) = NaN;
+        Hn(logical(kron([0,0;1,0],eye(6)))) = NaN;
 
-    % Invert for regression
-    IX = pinv(X);
+        Haa = kron(ones(2),H_);
+        Haa(logical(kron([0,1;1,1],ones(6)))) = NaN;
+        Haa(triu(true(12))) = NaN;
 
-    % Set/update prev_imgPerm
-    prev_imgPerm = imgPerm;
+        Hbb = kron(ones(2),H_);
+        Hbb(logical(kron([1,1;1,0],ones(6)))) = NaN;
+        Hbb(triu(true(12))) = NaN;
 
-end
-%get the 12x12 RSM for the searchlight image
-R = corr(M);
+        Hp = sum(cat(3,Haa,Hbb),3,'omitmissing');
+        Hp(isnan(Haa) & isnan(Hbb)) = NaN;
 
-%% prep y column of design mat
-%select out colcoation based vals of R and zscore within aa/bb/ba comp type
-yn = zscore([R(S.n.ab);R(S.n.ba)]);
-yp.a = zscore(R(S.p.a));
-yp.b = zscore(R(S.p.b));
-y = [yn;yp.a;yp.b];
+        % Make the selectors for the real data RSM
+        S.n = ~isnan(Hn);
+        S.aa = ~isnan(Haa);
+        S.bb = ~isnan(Hbb);
+        S.p = ~isnan(Hp);
+        S.all = S.n | S.p;
 
-%do regression and Fischer-transform the results
-z = atanh(IX*y);
-return
+        % Load in predicted visual similarity from denseNet-169
+        V = VisSim(imgPerm,imgPerm);
+        V = kron(ones(2),V);
+
+        % Construct the design matrix
+        X = [...
+            nanzscore(Hn(S.all)),...
+            nanzscore(Hp(S.all)),...
+            nanzscore(V(S.all))];
+
+        % Replace N/A values with mean
+        X(isnan(X)) = 0;
+
+        % Invert for regression
+        IX = pinv(X);
+
+        % Set/update prev_imgPerm
+        prev_imgPerm = imgPerm;
+
+    end
+
+    % Get the 12x12 RSM for the searchlight image
+    R = corr(M);
+
+    % Zscore within aa/bb/ba pairs
+    R(S.n) = nanzscore(R(S.n));
+    R(S.aa) = nanzscore(R(S.aa));
+    R(S.bb) = nanzscore(R(S.bb));
+    y = R(S.all);
+
+    % Do regression and Fischer-transform the results
+    z = atanh(IX*y);
+    return
