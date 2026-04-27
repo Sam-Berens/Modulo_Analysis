25688602db694a7bb244d697a40f3762692196c0
sophie <sj483@sussex.ac.uk>
2026-04-27 12:04:47 +0100

    Finished writing A02/z04_estim2ndLvlPPIs.m

--- A02/z04_estim2ndLvlPPIs.m
+++ A02/z04_estim2ndLvlPPIs.m
@@ -1,25 +1,38 @@
-function [] = z04_estim2ndLvlPPIs(G)
-% Pick which contrast numbers you want to estimate models for and build a
-% vector of them
-% cons 4-7 are the two PPI effects, the main effect of PPI (irrespective of
-% a or b condition) and the difference between the two PPI effects 
-for ii=4:7
-    estim2ndLvl(ii);
-end
+function [] = z04_estim2ndLvlPPIs(G,roiInfo)
+%% Estimates a seperate 2nd level model each for 4 contrast, for a given roi 
+% cons are the two PPI effects, the main effect of PPI (irrespective of
+% a or b condition) and the difference between the two PPI effects
 
+% roiInfo.fn = '/mnt/Erebus/Modulo/Data/_Group/MniRois/_Cluster-Alpha01-Mdl05a_+zPnonc_rVisual.nii';
+% roiInfo.id = 'rVisual';
+% Pick which contrast numbers you want to estimate models for and build a
+% vector of them 
+for ii=4:7
+    estim2ndLvl(G,roiInfo,ii);
+end
 return
 
+function [] = estim2ndLvl(G,roiInfo,con)
 
-%TO DO - finish below!!
-
-function [] = estim2ndLvl(con,G)
-%construct scans cell aray doing like 
 subjectIds = sort(getSubjectIds(G));
-nSubs = numel(subjectIds);
+dir_Data = ['..',filesep,'..',filesep,'Data'];
+dir_G = fullfile(dir_Data,'_Group','G1');
+epiMask = fullfile(dir_G,'Structural','GrpEpiMask00',...
+    sprintf('%s_GrpEpiMask00.nii',G));
+dir_outPut = fullfile(dir_Data,'_Group',G,'Analysis','A02',sprintf('con_%03d',con));
+%check if A02 group dir exists, and then if the subdir for that contrast
+%model exists before making them
+if ~exist(dir_outPut(1:end-8),"dir")
+    mkdir(dir_outPut(1:end-8))
+end
+if ~exist(dir_outPut,"dir")
+    mkdir(dir_outPut)
+end
 
-dirs = arrayfun(@(x) dirFnc(x), subjectIds,'UniformOutput',false);
-scans = cellfun(@(x) sprintf('%s%s%0d3.nii',x,...
-    filesep,con),dirs.PPI,'UniformOutput',false);
+%construct scans cell array 
+dirs = arrayfun(@(x) dirFnc(x,dir_Data,roiInfo), subjectIds,'UniformOutput',false);
+scans = cellfun(@(x) sprintf('%s%scon_%04d.nii',x.PPI,...
+    filesep,con),dirs,'UniformOutput',false);
 %get covariate
 pNonc = get_pNonc(G);
 pNonc.zpNonc = zscore(pNonc.pNonc);
@@ -28,7 +41,7 @@ pNonc.Properties.RowNames = cellstr([pNonc.subjectId]');
 pNonc = sortrows(pNonc);
 zpNonc = pNonc{:,'zpNonc'};
 
-spmBatch{1}.spm.stats.factorial_design.dir = {'/mnt/Erebus/Modulo/Data/_Group/G1/Analysis'};
+spmBatch{1}.spm.stats.factorial_design.dir = {dir_outPut};
 spmBatch{1}.spm.stats.factorial_design.des.t1.scans = scans;
 spmBatch{1}.spm.stats.factorial_design.cov.c = zpNonc;
 spmBatch{1}.spm.stats.factorial_design.cov.cname = 'zpNonc';
@@ -44,9 +57,27 @@ spmBatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
 spmBatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
 
 % Estimation
-spmBatch{2}.spm.stats.fmri_est.spmmat = {[dirs.Output,filesep,'SPM.mat']};
+spmBatch{2}.spm.stats.fmri_est.spmmat = {fullfile(dir_outPut,'SPM.mat')};
 spmBatch{2}.spm.stats.fmri_est.write_residuals = 0;
 spmBatch{2}.spm.stats.fmri_est.method.Classical = 1;
+
+% Make contrasts
+conNames = cell(2,1);
+%effect of intercept
+conNames{1} = 'intercept';
+H.intercept = 1;
+%main effect of pNonc
+conNames{2} = 'zPnonc';
+H.zPnonc = [0,1];
+fields = fieldnames(H);
+
+spmBatch{3}.spm.stats.con.spmmat = {fullfile(dir_outPut,'SPM.mat')};
+for iH = 1:numel(conNames)
+    spmBatch{3}.spm.stats.con.consess{iH}.tcon.name = conNames{iH};
+    spmBatch{3}.spm.stats.con.consess{iH}.tcon.weights = H.(fields{iH});
+    spmBatch{3}.spm.stats.con.consess{iH}.tcon.sessrep = 'replsc';
+end
+spmBatch{3}.spm.stats.con.delete = 1;
 
 % Run th job
 spm_jobman('initcfg');
@@ -55,10 +86,11 @@ spm_jobman('run',spmBatch);
 return
 
 
-function [dirs] = dirFnc(subjectId)
+function [dirs] = dirFnc(subjectId,dir_Data,roiInfo)
 subjectId = char(subjectId);
+dir_Data = char(dir_Data);
 %get subject-specific filenames and info
-dirs.Subject = fullfile(dirs_Data,subjectId);
+dirs.Subject = fullfile(dir_Data,subjectId);
 dirs.A02 = fullfile(dirs.Subject,'Analysis','A02');
 dirs.PPI = fullfile(dirs.A02,sprintf('PPI_%s',roiInfo.id));
 return
